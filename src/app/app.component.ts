e43a1838ef05b5690afcb96514a111e929616095
NataliKr-00 <63677755+NataliKr-00@users.noreply.github.com>
2020-07-12 13:06:06 +0300

    start

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+allowNewServer = false;
+constructor() {
+  setTimeout(() => {
+    this.allowNewServer = true;
+  },2000)
+}
+ngOnInit(){
+
+}
+}
